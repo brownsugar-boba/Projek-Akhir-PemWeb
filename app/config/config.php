aaae9667310dd5dcfab49117d2f2091ff5dfee60
Dinda Putri Ani <71748271+brownsugar-boba@users.noreply.github.com>
2021-06-17 10:12:56 +0700

    Delete app directory

--- app/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define ('BASEURL','http://localhost/pweb/Project_train/public');
-
-define ('DB_HOST','localhost');
-define ('DB_USER','root');
-define ('DB_PASS','');
-define ('DB_NAME','practice');
